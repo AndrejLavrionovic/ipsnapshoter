d93e8934e80a7600ddebb824494c7d41747bfdd1
AndrejLavrionovic <andrei.lavrinovic@gmail.com>
2016-08-23 09:01:31 +0100

    fl (filemodes) syntax corrected.

--- org/fc.py
+++ org/fc.py
@@ -2,28 +2,34 @@
 
 import flmds
 
+
 class fl:
-	'This us parent class for other classes that will work with files'
-	
-	__filename = "";
-	__filepath = "";
-	__filemode = "";
-	
-	def __init__(self, f="file.txt", m=0):
-		self.__md = flmds.fmd(m);
-		self.__filename = f;
-		self.__filemode = self.__md.getmd();
-	
-	def setfn(self, fn):
-		self.__filename = fn
-	def getfn(self):
-		return self.__filename
-	def setfp(self, fp):
-		self.__filepath = fp
-	def getfp(self):
-		return self.__filepath
-	def setfm(self, md):
-		self.__md.setmd(md);
-		self.__filemode = self.__md.getmd();
-	def getfm(self):
-		return self.__filemode;
+    """This us parent class for other classes that will work with files"""
+
+    __filename = ""
+    __filepath = ""
+    __filemode = ""
+
+    def __init__(self, f="file.txt", m=0):
+        self.__md = flmds.fmd(m)
+        self.__filename = f
+        self.__filemode = self.__md.getmd()
+
+    def setfn(self, fn):
+       self.__filename = fn
+
+    def getfn(self):
+        return self.__filename
+
+    def setfp(self, fp):
+        self.__filepath = fp
+
+    def getfp(self):
+        return self.__filepath
+
+    def setfm(self, md):
+        self.__md.setmd(md)
+        self.__filemode = self.__md.getmd()
+
+    def getfm(self):
+        return self.__filemode
